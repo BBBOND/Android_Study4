78bb79cdc01f3f3ed5d3b04a48ed8f2f3804ce00
BBBOND <jwy8645@163.com>
2016-02-09 14:29:18 +0800

    上拉加载更多的ListView

--- ListViewLoad/src/main/java/com/kim/android_study4/MainActivity.java
+++ ListViewLoad/src/main/java/com/kim/android_study4/MainActivity.java
@@ -1,13 +1,65 @@
 package com.kim.android_study4;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import com.kim.android_study4.view.LoadListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements LoadListView.LoadListener {
+
+    private LoadListView listView;
+    private ArrayAdapter<String> adapter = null;
+    private List<String> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (LoadListView) findViewById(R.id.listView);
+        listView.setInterface(this);
+
+        init();
+    }
+
+    private void init() {
+        list = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            list.add("item" + i);
+        }
+        showListView(list);
+    }
+
+    private void showListView(List<String> list) {
+        if (adapter == null) {
+            adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, list);
+            listView.setAdapter(adapter);
+        } else {
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+    @Override
+    public void onLoad() {
+        //获取更多数据
+        //更新listview显示
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 5; i++) {
+                    list.add("more item" + i);
+                }
+                showListView(list);
+                listView.loadComplete();
+            }
+        }, 2000);
+
     }
 }
