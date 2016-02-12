a00e477cc594796f61060581dd44c8c70abea964
BBBOND <jwy8645@163.com>
2016-02-12 22:21:57 +0800

    animator的各种动画

---
+++ animator/src/main/java/com/kim/animator/MainActivity.java
@@ -0,0 +1,97 @@
+package com.kim.animator;
+
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+    }
+
+    public void click(View view) {
+        Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * 旧的Animation方法
+     * 缺点:
+     * 1. view移动动作响应范围未移动
+     *
+     * @param view
+     */
+    public void move(View view) {
+        Toast.makeText(this, "moving...", Toast.LENGTH_SHORT).show();
+        TranslateAnimation animation = new TranslateAnimation(0, 200, 0, 0);
+        animation.setDuration(1000);
+        animation.setFillAfter(true);
+        imageView.startAnimation(animation);
+    }
+
+    /**
+     * 新的ObjectAnimator方法
+     * 特点:
+     * 1. view移动动作响应范围同时跟着移动
+     *
+     * @param view
+     */
+    public void objectAnimatorMove(View view) {
+        Toast.makeText(this, "moving...", Toast.LENGTH_SHORT).show();
+        //translationX、translationY 偏移量X、Y
+        //X、Y  最终所到达的绝对值
+        //rotation 旋转角度
+        ObjectAnimator.ofFloat(imageView, "rotation", 0f, 360f).setDuration(2000).start();
+        ObjectAnimator.ofFloat(imageView, "translationX", 0f, 200f).setDuration(2000).start();
+        ObjectAnimator.ofFloat(imageView, "translationY", 0f, 200f).setDuration(2000).start();
+    }
+
+    /**
+     * 新的PropertyValuesHolder设置变换方法及参数
+     * 特点:
+     * 1. 相对于直接使用ObjectAnimator方法资源更优化
+     *
+     * @param view
+     */
+    public void propertyValuesHolderMove(View view) {
+        Toast.makeText(this, "moving...", Toast.LENGTH_SHORT).show();
+        PropertyValuesHolder p1 = PropertyValuesHolder.ofFloat("rotation", 0f, 360f);
+        PropertyValuesHolder p2 = PropertyValuesHolder.ofFloat("translationX", 0f, 200f);
+        PropertyValuesHolder p3 = PropertyValuesHolder.ofFloat("translationY", 0f, 200f);
+        ObjectAnimator.ofPropertyValuesHolder(imageView, p1, p2, p3).setDuration(2000).start();
+    }
+
+    /**
+     * 新的AnimatorSet方法，可以顺序执行Animator动画
+     * 特点:
+     * 1. 可以按指定的顺序执行动画，而不是同时执行
+     * <p/>
+     * 注意:
+     * 1. Animator不可重用
+     *
+     * @param view
+     */
+    public void animatorSetMove(View view) {
+        Toast.makeText(this, "moving...", Toast.LENGTH_SHORT).show();
+        ObjectAnimator animator1 = ObjectAnimator.ofFloat(imageView, "rotation", 0f, 360f);
+        ObjectAnimator animator2 = ObjectAnimator.ofFloat(imageView, "translationX", 0f, 200f);
+        ObjectAnimator animator3 = ObjectAnimator.ofFloat(imageView, "translationY", 0f, 200f);
+        AnimatorSet set = new AnimatorSet();
+        set.play(animator2).with(animator3).before(animator1);
+//        set.playTogether(animator1, animator2, animator3);
+//        set.playSequentially(animator1, animator2, animator3);
+        set.setDuration(2000).start();
+    }
+}
