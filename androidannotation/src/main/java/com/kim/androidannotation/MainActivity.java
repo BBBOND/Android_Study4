d091a8172bb3084d92300b78f70daeeee12426d9
BBBOND <jwy8645@163.com>
2016-02-19 22:23:39 +0800

    使用框架实现按钮点击事件和Service

---
+++ androidannotation/src/main/java/com/kim/androidannotation/MainActivity.java
@@ -0,0 +1,36 @@
+package com.kim.androidannotation;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.kim.androidannotation.service.MyService_;
+
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends AppCompatActivity {
+
+//    @ViewById(R.id.toSecond)
+//    Button toSecond;
+//    @ViewById(R.id.startService)
+//    Button startService;
+
+    @Click(R.id.toSecond)
+    public void toSecondActivity() {
+        Intent intent = new Intent(MainActivity.this, SecondActivity_.class);
+        startActivity(intent);
+    }
+
+    @Click(R.id.startService)
+    public void startService(){
+        Intent intent = new Intent(MainActivity.this, MyService_.class);
+        startService(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ androidannotation/src/main/java/com/kim/androidannotation/service/MyService.java
@@ -0,0 +1,30 @@
+package com.kim.androidannotation.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.kim.androidannotation.SecondActivity_;
+
+import org.androidannotations.annotations.EService;
+
+/**
+ * Created by 伟阳 on 2016/2/19.
+ */
+@EService
+public class MyService extends Service {
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("MyService", "onStartCommand");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
