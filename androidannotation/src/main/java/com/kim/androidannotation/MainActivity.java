02eaad9bd3550a7850bd882ae7859bbef27b4c90
BBBOND <jwy8645@163.com>
2016-02-20 20:12:01 +0800

    1. @ViewById命名符合规格就可以不用加ID 2. @ViewById参数为Id数组将view存入list中 3. @AfterViews在View绘制完之后执行

--- androidannotation/src/main/java/com/kim/androidannotation/MainActivity.java
+++ androidannotation/src/main/java/com/kim/androidannotation/MainActivity.java
@@ -3,19 +3,31 @@ package com.kim.androidannotation;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.kim.androidannotation.service.MyService_;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+import org.androidannotations.annotations.ViewsById;
+
+import java.util.List;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
 
-//    @ViewById(R.id.toSecond)
-//    Button toSecond;
-//    @ViewById(R.id.startService)
-//    Button startService;
+    @ViewById(R.id.toSecond)
+    Button toSecond;
+    @ViewById(R.id.startService)
+    Button startService;
+    //命名符合规格就可以不用加ID
+    @ViewById
+    TextView tvHello;
+    @ViewsById({R.id.text1, R.id.text2, R.id.text3})
+    List<TextView> list;
 
     @Click(R.id.toSecond)
     public void toSecondActivity() {
@@ -27,6 +39,14 @@ public class MainActivity extends AppCompatActivity {
     public void startService() {
         Intent intent = new Intent(MainActivity.this, MyService_.class);
         startService(intent);
+    }
+
+    @AfterViews
+    public void showTextView() {
+        tvHello.setText("Hello World!");
+        for (int i = 0; i < list.size(); i++) {
+            list.get(i).setText("text " + i);
+        }
     }
 
     @Override
